8e8e57648f50b27feb0be7109c4cc91dbe2bddc6
What is my name <65349891+DarkKingStar@users.noreply.github.com>
2022-06-30 22:48:55 +0530

    Update read.c

--- program 4/read.c
+++ program 4/read.c
@@ -1,23 +1,27 @@
+// Write a programme in C to perform multiple time one-way communication b/w two independent process by using named pipe..
+
+// saved the file as Read.c
+
 #include<stdio.h>
-#include<sys/types.h>
 #include<unistd.h>
 #include<string.h>
-#include<fcntl.h>
+#include<sys/types.h>
 #include<sys/stat.h>
-#include<string.h>
-#include<stdlib.h>
-
-void main()
+#include<fcntl.h>
+int main()
 {
     int fd;
+    char *myfifo="fifo";
     char buff[1024];
-    char * myfifo= "fifo";
     mkfifo(myfifo,0666);
-    fd = open(myfifo, O_RDONLY);
-    while(1){
+    fd=open(myfifo,O_RDONLY);
+    while(1)
+    {
         read(fd,buff,1024);
-        printf("Reader received String: \t%s\n",buff);
-        if(strcmp(buff,"exit")==0)break;
+        printf("Received Data:%s\n",buff);
+        if(strcmp(buff,"exit")==0)
+        break;
     }
     close(fd);
+    return 0;
 }
