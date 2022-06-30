18be1c38fbeb4571ad44c715689972f3de64fb3e
What is my name <65349891+DarkKingStar@users.noreply.github.com>
2022-06-30 19:16:26 +0530

    Update tcp_server.c

--- program 6/tcp_server.c
+++ program 6/tcp_server.c
@@ -1,32 +1,38 @@
 #include<stdio.h>
 #include<string.h>
-#include<sys/types.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
 #define PORT 8080
-void main(){
-    int svrsock_fd,new_conn;
-    char message[1024],buff[1024];
-    int opt=1;
-    struct sockaddr_in svraddr;
-    socklen_t addrlen=sizeof(struct sockaddr_in);
-    svrsock_fd=socket(AF_INET,sock_STREAM,0);
-    setsockopt(svrsock_fd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,&opt,sizeof(opt));
-    svraddr.sin_family=AF_INET;
-    svraddr.sin_addr.s_addr=INADDR_ANY;
-    svraddr.sin_port=htons(PORT);
-    
-    bind(svrsock_fd,(struct sockaddr *)& address,addrlen);
-    listen(svrsock_fd,3);
-    printf("Waiting for Client\n");
-    new_conn=accept(svrsock_fd,(struct sockaddr *)& address,&addrlen);
-    while(1){
-        read(new_conn,buff,1024);
-        printf("Received message for client:%s\n",buff);
-        if(strcmp(buff,"exit")==0)break;
-        printf("enter string for client:");
-        gets(message);
-        send(new_conn,message,sizeof(message),0);
-        if(strcmp(message,"exit")==0)break;
-    }
+
+void main()
+{
+	int svrsock_fd,new_conn;
+	char message[1024],buff[1024];
+	int opt=1;
+	struct sockaddr_in address;
+	socklen_t addrlen=sizeof(struct sockaddr_in);
+	svrsock_fd=socket(AF_INET,SOCK_STREAM,0);
+	address.sin_family=AF_INET;
+	address.sin_addr.s_addr=INADDR_ANY;
+	address.sin_port=htons(PORT);
+	setsockopt(svrsock_fd,SOL_SOCKET,SO_REUSEADDR|SO_REUSEPORT,&opt,sizeof(opt));
+	bind(svrsock_fd,(struct sockaddr *)&address,addrlen);
+	listen(svrsock_fd,3);
+	printf("Waiting for client\n");
+	new_conn=accept(svrsock_fd,(struct sockaddr*)&address,&addrlen);
+
+	while(1)
+	{
+		memset(buff,0,1024);
+		read(new_conn,buff,1024);
+		printf("Received message from client:%s\n",buff);
+		if(strcmp(buff,"end")==0)
+		{
+			send(new_conn,"OK",3,0);
+			break;
+		}
+		printf("Enter message for client:");
+		gets(message);
+		send(new_conn,message,strlen(message),0);
+	}
 }
